ba3029e1ead4f577e9dc09081f4104dd2279529a
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-14 16:21:10 -0400

    rendered the new contact page and lionked the route and controller to it

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :contacts
+  # Shorthand that creates routes for our class. check the rotues in the command by rails routes
 end
